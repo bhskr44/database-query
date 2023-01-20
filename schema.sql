f5c58fa1fe326bf38d9c96f65bf11ef4765c3021
BHASKAR RANJAN BORA <bhaskarranjan44@gmail.com>
2023-01-21 00:41:37 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -73,6 +73,42 @@ CREATE TABLE vets (
     age INT,
     date_of_graduation DATE
 );
+--  Create a "join table" called specializations to handle this relationship.
+
+CREATE TABLE specializations (
+    id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(id),
+    vet_id INT REFERENCES vets(id)
+);
+
+-- Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE
+);
+
+-- Insert the following data for vets:
+
+-- Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+
+INSERT INTO vets (name, age, date_of_graduation)	VALUES ('William Tatcher', 45, '2000-04-23');
+
+-- Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+
+INSERT INTO vets (name, age, date_of_graduation)	VALUES ('Maisy Smith', 26, '2019-01-17');
+
+-- Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+
+INSERT INTO vets (name, age, date_of_graduation)	VALUES ('Stephanie Mendez', 64, '1981-05-04');
+
+-- Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+
+INSERT INTO vets (name, age, date_of_graduation)	VALUES ('Jack Harkness', 38, '2008-06-08');
+
+
+
 
 
 
