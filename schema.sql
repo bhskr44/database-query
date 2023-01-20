e839a217274057e811139cdf4f3079f5234b90ed
BHASKAR RANJAN BORA <bhaskarranjan44@gmail.com>
2023-01-21 00:29:46 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,54 @@ Create table animals (
 
 ALTER TABLE animals
 ADD species Text;
+
+
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer
+
+CREATE TABLE owners (
+	id integer,
+	full_name varchar,
+	age integer
+)
+
+
+
+-- Create a table named species with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+
+
+CREATE TABLE species (
+	id integer,
+	name varchar
+)
+
+
+-- Modify animals table:
+-- Make sure that id is set as autoincremented PRIMARY KEY
+-- Remove column species
+-- Add column species_id which is a foreign key referencing species table
+-- Add column owner_id which is a foreign key referencing the owners table
+
+CREATE TABLE new_animals (
+    id SERIAL PRIMARY KEY,
+   name varchar,
+	date_of_birth date,
+	escape_attempts integer,
+	neutered BOOLEAN,
+	weight_kg decimal,
+    species_id INT REFERENCES species(id),
+    owner_id INT REFERENCES owners(id)
+);
+
+INSERT INTO new_animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, owner_id)
+SELECT name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, owner_id
+FROM animals;
+
+DROP TABLE animals;
+
+ALTER TABLE new_animals RENAME TO animals;
+
