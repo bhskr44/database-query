72c7bb5b784fc304543bf486e7d21006fc290971
BHASKAR RANJAN BORA <bhaskarranjan44@gmail.com>
2023-01-21 00:29:39 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -15,4 +15,71 @@ INSERT INTO public.animals(id, name, date_of_birth, escape_attempts, neutered, w
 	(10,'Blossom', '10/13/1998', 3, true, 17),
 	(11,'Ditto', '05/14/2022', 4, true, 22);
 
+-- 	Insert the following data into the owners table:
+-- Sam Smith 34 years old.
+-- Jennifer Orwell 19 years old.
+-- Bob 45 years old.
+-- Melody Pond 77 years old.
+-- Dean Winchester 14 years old.
+-- Jodie Whittaker 38 years old.
+
+INSERT INTO public.owners(id, full_name, age)
+	VALUES  
+	(1,'Sam Smith', 34),
+	(2,'Jennifer Orwell', 19),
+	(3,'Bob', 45),
+	(4,'Melody Pond', 77),
+	(5,'Dean Winchester', 14),
+	(6,'Jodie Whittaker', 38);
+
+
+-- 	Insert the following data into the species table:
+-- Pokemon
+-- Digimon
+
+INSERT INTO public.species(id, name)
+	VALUES  
+	(1,'Pokemon'),
+	(2,'Digimon');
+
+-- 	Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+-- Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals
+SET owner_id = 2
+WHERE name = 'Gabumon' or name = 'Pikachu';
+
+-- Bob owns Devimon and Plantmon.
+
+UPDATE animals
+SET owner_id = 3
+WHERE name = 'Devimon' or name = 'Plantmon';
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Charmander' or name = 'Squirtle' or name = 'Blossom';
+
+-- Dean Winchester owns Angemon and Boarmon.
+
+UPDATE animals
+SET owner_id = 5
+WHERE name = 'Angemon' or name = 'Boarmon';
+
+
+
+
+
 	
